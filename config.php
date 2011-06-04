3aafedd9a0ca3c1d721ed0e832538914595d4bfe
Ricardo Ramirez <ricardo@theclickpro.com>
2011-06-04 19:20:40 +0000

    Adjusted config file

--- config.php
+++ config.php
@@ -1,30 +1,28 @@
 <?php
 //
-$CFG['app_name'] 		= 	'Repository Admnistrator';
+$CFG['app_name'] 			= 	'Repository Admnistrator';
 
 //
-$CFG['password_file'] 		= 	dirname(__FILE__).'/password';
+$CFG['password_file'] 			= 	dirname(__FILE__).'/password';
 
 //
-$CFG['env']			= 	'/usr/bin/env';
-
+$CFG['env']				= 	'/usr/bin/env';
 
 // apache
-$CFG['htpasswd'] 		= 	"/usr/bin/htpasswd";
+$CFG['htpasswd'] 			= 	"/usr/bin/htpasswd";
 
 // svn
-$CFG['repos']['svn']['uri']	= 	'http://dev.theclickpro.com/svn';
+$CFG['repos']['svn']['uri']		= 	'http://dev.theclickpro.com/svn';
 $CFG['repos']['svn']['svnadmin']	= 	'/usr/bin/svnadmin';
-$CFG['repos']['svn']['dir']	= 	dirname(__FILE__) .'/data/svn';
+$CFG['repos']['svn']['dir']		= 	dirname(__FILE__) .'/data/svn';
 
 //
-$CFG['repos']['git']['git']	= 	'/usr/bin/git';
-$CFG['repos']['git']['uri']	= 	'http://dev.theclickpro.com/git';
-$CFG['repos']['git']['dir']	= 	dirname(__FILE__) .'/data/git';
+$CFG['repos']['git']['git']		= 	'/usr/bin/git';
+$CFG['repos']['git']['uri']		= 	'http://dev.theclickpro.com/git';
+$CFG['repos']['git']['dir']		= 	dirname(__FILE__) .'/data/git';
 
 
-
-$CFG['trash']			=	dirname(__FILE__)."/trash";
-$CFG['storage']			=	dirname(__FILE__).'/data/db';
+$CFG['trash']				=	dirname(__FILE__)."/trash";
+$CFG['storage']				=	dirname(__FILE__).'/data/db';
 
 
