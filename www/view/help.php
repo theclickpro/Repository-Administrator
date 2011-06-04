0671363619eaa702c1dbfca72d37e9e29a86b0c8
Ricardo Ramirez <ricardo@theclickpro.com>
2011-06-04 19:34:01 +0000

    Added GPL legend to the header of all files in source code

--- www/view/help.php
+++ www/view/help.php
@@ -1,3 +1,23 @@
+<?php
+/*
+Copyright 2011 Ricardo Ramirez, The ClickPro.com LLC
+
+This file is part of Repository Administrator.
+
+Repository Administrator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Repository Administrator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Repository Administrator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
 <h2>Apache Configuration</h2>
 <p>This configuration has been tested in Ubuntu Linux with Apache 2, but it should work in must modern distros.</p>
 
--- www/view/users_edit.php
+++ www/view/users_edit.php
@@ -1,4 +1,23 @@
 <?php
+/*
+Copyright 2011 Ricardo Ramirez, The ClickPro.com LLC
+
+This file is part of Repository Administrator.
+
+Repository Administrator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Repository Administrator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Repository Administrator.  If not, see <http://www.gnu.org/licenses/>.
+*/?>
+<?php
 	$row = $this->row;
 ?>
 <form method="post" action="<?php echo BASEURL?>/do/users_save.php">
